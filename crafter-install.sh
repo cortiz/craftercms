1ee9b1921bd6f844a1b484251dd08baf321fa60a
Carlos Ortiz <carlos.ortiz@craftercms.org>
2018-02-16 19:25:22 -0600

    V1.1 Reject Root user
    Signed-off-by: Carlos Ortiz <carlos.ortiz@craftercms.org>

--- crafter-install.sh
+++ crafter-install.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+
+if [ "$(whoami)" == "root" ]; then
+	echo -e "\033[38;5;196m"
+	echo -e "Crafter CMS cowardly refuses to run as root."
+    echo -e "Running as root is dangerous and is not supported."
+    echo -e "\033[0m"
+	exit 1
+fi
+
 DEFAULT_VERSION="$1"
 DEFAULT_INSTALL_DIR="$(pwd)" ##DELETE THIS
 
